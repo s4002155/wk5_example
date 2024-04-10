d9b8023c432da968d14e145335b2d20e8863b80b
s4002155 <s4002155@student.rmit.edu.au>
2024-04-10 10:56:47 +1000

    print function

--- public/script.js
+++ public/script.js
@@ -1,22 +1,45 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
-const cnv = document.getElementById (`cnv_element`)
-cnv.width = innerWidth
-cnv.height = innerHeight
+// const cnv = document.getElementById (`cnv_element`)
+// cnv.width = innerWidth
+// cnv.height = innerHeight
 
-const ctx = cnv.getContext (`2d`)
+// const ctx = cnv.getContext (`2d`)
 
-const draw_frame = () => {
-   ctx.fillStyle = `turquoise`
-   ctx.fillRect (0, 0, innerWidth, innerHeight)
+// const draw_frame = () => {
+//    ctx.fillStyle = `turquoise`
+//    ctx.fillRect (0, 0, innerWidth, innerHeight)
 
-   requestAnimationFrame (draw_frame)
-}
+//    requestAnimationFrame (draw_frame)
+// }
 
-draw_frame ()
+// draw_frame ()
 
-window.onresize = () => {
-   cnv.width = innerWidth
-   cnv.height = innerHeight   
-}
+// window.onresize = () => {
+//    cnv.width = innerWidth
+//    cnv.height = innerHeight   
+// }
+
+
+
+
+// function print (msg)  {
+//    console.log (msg)
+// }
+
+// const print = msg => {
+//    console.log (msg)
+// } 
+
+// const print = (msg) => console.log (msg)
+
+const print = msg => console.log (msg)
+
+print ('whats up')
+
+const ex_arr = [ 'a'. 'b', 'c' ]
+
+print (ex_arr.length)
+
+ex_arr.forEach (derrick => console.log (derrick))
